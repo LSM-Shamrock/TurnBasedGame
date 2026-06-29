112be1059763c29a6c9776e5f5569ff2e238a019
LSM-Shamrock <167507014+LSM-Shamrock@users.noreply.github.com>
2026-06-29 09:48:09 +0900

    .

--- Assets/01_Scripts/Player.cs
+++ Assets/01_Scripts/Player.cs
@@ -7,39 +7,44 @@ public class Player : MonoBehaviour, ISceneInstance<Player>
 {
     [SerializeField, ComponentField]
     private Rigidbody2D _rigidbody;
-    [SerializeField, ChildField("GroundChecker")]
-    private Collider2D _groundChecker;
-    
+
+    private float _groundCheckRadius = 0.2f;
+    private string _groundLayerName = "Ground";
+    private LayerMask _groundLayer;
+
     private float _moveSpeed = 3f;
     private float _fallSpeed = 1f;
     private float _jumpPower = 10f;
 
     private float _moveInput;
     private bool _jumpInput;
-    private bool _isGround;
 
     private void Start()
     {
+        _groundLayer = LayerMask.GetMask(_groundLayerName);
+
         ((ISceneInstance<Player>)this).InitSceneInstance();
     }
+
     private void Update()
     {
         _moveInput = Input.GetAxisRaw("Horizontal");
-        _jumpInput = Input.GetKeyDown(KeyCode.Space);
+        _jumpInput = Input.GetKey(KeyCode.Space);
     }
+
     private void FixedUpdate()
     {
         _rigidbody.linearVelocityY -= _fallSpeed;
-
         _rigidbody.linearVelocityX = _moveInput * _moveSpeed;
 
-        _isGround = CheckGround();
-        if (_isGround && _jumpInput)
+        if (_jumpInput && IsGrounded())
+        {
             _rigidbody.linearVelocityY = _jumpPower;
+        }
     }
 
-    private bool CheckGround()
+    private bool IsGrounded()
     {
-        return Physics2D.OverlapBox(_groundChecker.bounds.center, _groundChecker.bounds.size, _groundChecker.includeLayers);
+        return Physics2D.OverlapCircle(transform.position, _groundCheckRadius, _groundLayer);
     }
 }
